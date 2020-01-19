e5c21895f4c63106ff913fdc8223750d00d3eaa7
Robert Rong <robert.rong@agile-solutions.ch>
2020-01-19 13:10:32 +0100

    f

--- src/main/java/ch/agilesolutions/boot/repository/UserRepository.java
+++ src/main/java/ch/agilesolutions/boot/repository/UserRepository.java
@@ -1,23 +1,12 @@
 package ch.agilesolutions.boot.repository;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import ch.agilesolutions.boot.model.User;
 
-@Transactional(readOnly = true)
-public interface UserRepository extends JpaRepository<User, Integer> {
 
-	@Transactional(timeout = 10)
-	User findByUserId(Integer carId);
-	
-	@Transactional(timeout = 10)
-	List<User> findAll();
-	
-	@Transactional
-	<S extends User> S save(User car);
-	
-	void delete(User car);
+@RepositoryRestResource
+public interface UserRepository extends CrudRepository <User, Integer> {
 }
